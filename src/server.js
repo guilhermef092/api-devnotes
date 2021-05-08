98efc99a7c3d89f03dd4334c92cd996c449279ad
Guilherme Figueiredo <guilherme.figueiredo092@gmail.com>
2021-05-08 16:14:11 -0300

    Config project

--- src/server.js
+++ src/server.js
@@ -3,12 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const routes = require('./routes');
+
 const server = express();
 server.use(cors());
-
-server.get('/ping', (req, res) => {
-  res.json({ pong: true });
-});
+server.use('/api', routes);
 
 server.listen(process.env.PORT, () => {
   console.log(`Servidor rodando na porta: ${process.env.PORT}`);
