9eb55c5bbb4af6e3add14b5cef8d1e7e8e5a8950
Guilherme Figueiredo <guilherme.figueiredo092@gmail.com>
2021-05-09 07:59:53 -0300

    Routes

--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,10 @@ const NoteController = require('./controllers/NoteController');
 
 router.get('/ping', NoteController.ping);
 
+router.get('/notes', NoteController.all);
+router.get('/notes/:id', NoteController.one);
+router.post('/notes', NoteController.new);
+router.put('/notes/:id', NoteController.edit);
+router.delete('/notes/:id', NoteController.delete);
+
 module.exports = router;
